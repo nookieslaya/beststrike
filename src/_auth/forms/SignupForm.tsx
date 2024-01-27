6cdd76836be791ec03011d932cb5dfb7d1d9a06e
Radek <radek.dumin@gmail.com>
2024-01-27 16:08:40 +0100

    db settings

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,4 +1,5 @@
 import {Button} from "@/components/ui/button"
+import {Link} from "react-router-dom"
 import {
     Form,
     FormControl,
@@ -11,13 +12,16 @@ import {
 import {Input} from "@/components/ui/input"
 import {zodResolver} from "@hookform/resolvers/zod"
 import {useForm} from "react-hook-form"
-import {SingupValidation} from "@/lib/validation";
+import {SignupValidation} from "@/lib/validation";
 import {z} from "zod";
+import {Loader} from "../../components/shared/Loader.tsx"
+import {createUserAccount} from "@/lib/appwrite/api.ts";
 
 
 export const SignupForm = () => {
-    const form = useForm<z.infer<typeof SingupValidation>>({
-        resolver: zodResolver(SingupValidation),
+    const isLoading = false
+    const form = useForm<z.infer<typeof SignupValidation>>({
+        resolver: zodResolver(SignupValidation),
         defaultValues: {
             name: "",
             username: "",
@@ -27,10 +31,10 @@ export const SignupForm = () => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof SingupValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    async function onSubmit(values: z.infer<typeof SignupValidation>) {
+
+        const newUser = await createUserAccount(values)
+        console.log(newUser)
     }
 
 
@@ -38,14 +42,14 @@ export const SignupForm = () => {
         <>
             <Form {...form}>
                 <div className="sm:w-420 flex-center flex-col">
-                    <img src="/assets/images/logo.svg" alt="logo"/>
+                    <img src="/assets/images/logo2.svg" alt="logo"/>
                     <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
                     <p className="text-light-3 small-medium md:base-regular mt-2">To use BestStrike enter your account
                         details</p>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
                         <FormField
                             control={form.control}
-                            name="username"
+                            name="name"
                             render={({field}) => (
                                 <FormItem>
                                     <FormLabel>Name</FormLabel>
@@ -59,7 +63,64 @@ export const SignupForm = () => {
                                 </FormItem>
                             )}
                         />
-                        <Button type="submit">Submit</Button>
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input type="text" className="shad-input" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is your public display name.
+                                    </FormDescription>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />  <FormField
+                            control={form.control}
+                            name="email"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input type="email" className="shad-input" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is your public display name.
+                                    </FormDescription>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        /><FormField
+                            control={form.control}
+                            name="password"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" className="shad-input" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is your public display name.
+                                    </FormDescription>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" className="shad-button_primary">
+                        {isLoading ? (
+                            <div className="flex-center gap-2">
+                                <Loader/> Loading..
+                            </div>
+                        ): "Sign up"
+                        }
+                        </Button>
+                        <p className="text-small-regulat text-light-2 text-center mt-2">
+                            Already have an account
+                            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+                        </p>
                     </form>
                 </div>
 
